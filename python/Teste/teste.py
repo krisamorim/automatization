e90d5ca148208b5595b55f6427e276f5abb6e73c
kris.amorim <kris.a.furtado@gmail.com>
2024-03-24 10:33:51 -0300

    testando módulos de leitura e conversão de imagem

--- python/Teste/teste.py
+++ python/Teste/teste.py
@@ -1,14 +1,76 @@
-import os
+# import os
+# from PIL import Image, ExifTags
+# os.chdir(os.path.dirname(__file__))
 
-class Cliente:
-	def __init__(self, nome, email, plano):
-		self.nome = nome
-		self.email = email
-		lista_planos = ["basic", "premium"]
-		if plano in lista_planos:
-			self.plano = plano
-		else:
-			raise Exception("Plano inválido")
+# # Carregar a imagem
+# imagem = Image.open('down.jpg')
+
+# # Tentar obter os metadados da imagem
+# try:
+#     for orientacao in ExifTags.TAGS.keys():
+#         if ExifTags.TAGS[orientacao]=='Orientation':
+#             break
+#     exif = dict(imagem._getexif().items())
+
+#     if exif[orientacao] == 3:
+#         imagem = imagem.rotate(180, expand=True)
+#     elif exif[orientacao] == 6:
+#         imagem = imagem.rotate(270, expand=True)
+#     elif exif[orientacao] == 8:
+#         imagem = imagem.rotate(90, expand=True)
+# except (AttributeError, KeyError, IndexError):
+#     # Caso a imagem não tenha informações de orientação nos metadados
+#     print("Não foram encontradas informações de orientação nos metadados da imagem.")
+
+# # Salvar a imagem corrigida
+# imagem.save('imagem_corrigida.jpg')
+
+
+
+
+
+
+import cv2
+import os
+os.chdir(os.path.dirname(__file__))
+
+a = os.path.dirname(__file__)+r'\hEmDeiMeta.jpg'
+
+def detctar(caminho):
+	# Carregar a imagem
+	imagem = cv2.imread(caminho)
+
+	# Obter as dimensões da imagem
+	altura, largura = imagem.shape[:2]
+
+	# Se a largura é maior que a altura, então a imagem pode estar rotacionada
+	if largura > altura:
+	    print("A imagem pode estar rotacionada.")
+	else:
+		print("A imagem parece estar na orientação correta.")
+
+detctar(a)
+
+# from PIL import Image
+# from os import path
+# import os
+# a = path.dirname(__file__)+r'\a.jpg'
+# image = Image.open(a)
+# os.chdir(path.dirname(__file__))
+# image.save('b.png')
+
+    
+# import os
+
+# class Cliente:
+# 	def __init__(self, nome, email, plano):
+# 		self.nome = nome
+# 		self.email = email
+# 		lista_planos = ["basic", "premium"]
+# 		if plano in lista_planos:
+# 			self.plano = plano
+# 		else:
+# 			raise Exception("Plano inválido")
 		
-cliente = Cliente("Lira", "lira@gmail.com", "basic")
-print(cliente.nome)
+# cliente = Cliente("Lira", "lira@gmail.com", "basic")
+# print(cliente.nome)
