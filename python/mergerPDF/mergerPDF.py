371c623eb95b8f27fd2ef9c4bca7637f83d94884
kris.amorim <kris.a.furtado@gmail.com>
2024-04-04 09:18:02 -0300

    merger V2 -0 escolhe pasta

--- python/mergerPDF/mergerPDF.py
+++ python/mergerPDF/mergerPDF.py
@@ -5,11 +5,11 @@ merger = PyPDF2.PdfMerger()
 # pastaAtual = os.path.dirname(__file__)
 # lista_arquivos = os.listdir(pastaAtual)
 pastaDosArquivos = input('Qual o caminho da pasta?\n>>')
+finalNamefile = input('Qual nome vc deseja par ao arquivo final?\n>>')
 lista_arquivos = os.listdir(pastaDosArquivos)
 
 for arquivo in lista_arquivos:
     if ".pdf" in arquivo:
         pathCurrentFile = pastaDosArquivos+f'\\{arquivo}'
         merger.append(pathCurrentFile)
-
-merger.write(pastaDosArquivos+f'\\PDF_FINAL.pdf')
+merger.write(pastaDosArquivos+f'\\{finalNamefile}.pdf')
