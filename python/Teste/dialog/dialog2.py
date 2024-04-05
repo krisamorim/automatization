e23b6f796008cb7e3734af7f4386409f659ae1c6
kris.amorim <kris.a.furtado@gmail.com>
2024-04-05 14:00:51 -0300

    tela criada e configurada

--- python/Teste/dialog/dialog2.py
+++ python/Teste/dialog/dialog2.py
@@ -5,7 +5,16 @@ root = Tk()
 class Application():
     def __init__(self):
         self.root = root
+        self.tela()
         root.mainloop()
+
+    def tela(self):
+        self.root.title("Caminho dos arquivos")
+        self.root.configure(background= '#1e3743')
+        self.root.geometry('400x300')
+        self.root.resizable(True, True)
+        self.root.maxsize(width=500, height=500)
+        self.root.minsize(width=300, height=300)
 
 
 Application()
