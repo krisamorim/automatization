ba16ef229944e5e4a0d75a16dd85f8fe728bde32
kris.amorim <kris.a.furtado@gmail.com>
2025-09-03 10:51:08 -0300

    apontando horas com selelnium indo até carregar a pagina

--- empython/ApontHsNoDynamics/usingSelenium.py
+++ empython/ApontHsNoDynamics/usingSelenium.py
@@ -1,10 +1,106 @@
 # from selenium import webdriver
 # driver = webdriver.Firefox(executable_path='C:\Program Files\SeleniumMozila\geckodriver.exe')
 # driver.get("http://www.python.org")
-
+import password as pw
+import variables
+import time
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
-service = webdriver.FirefoxService(executable_path=r'C:\Program Files\SeleniumMozila\geckodriver.exe')
+
+# Configurações opcionais do Firefox
 options = webdriver.FirefoxOptions()
+
+# Inicializa o Firefox
+service = webdriver.FirefoxService(executable_path=r'C:\Program Files\SeleniumMozila\geckodriver.exe')
 driver = webdriver.Firefox(service=service, options=options)
+# options.add_argument("--start-maximized")  # inicia maximizado (não funciona no Windows)
+driver.maximize_window() # maximiza de verdade
+
+# Acessa a página desejada
 driver.get('https://linxprd.crm2.dynamics.com/main.aspx?pagetype=entitylist&etn=msdyn_workorder&viewid=63e0ccf4-8db5-e711-80f9-e0071b664ec1&viewType=1039&forceUCI=1&appid=0f6e740a-7a48-4536-872f-268ac257e0c7')
+time.sleep(5)  # espera a página carregar
+
+# Localiza elementos
+def localizar_elemento(driver, element_id: str, element_xpath: str):
+    """
+    Tenta localizar um elemento primeiro pelo ID, depois pelo XPath.
+    Faz 3 tentativas com intervalos crescentes.
+    Caso não encontre, pergunta ao usuário se deseja sair ou continuar.
+    """
+    
+    # Tentativas por ID
+    tentativas_id = [0, 2, 3]  # espera antes da tentativa
+    for espera in tentativas_id:
+        if espera > 0:
+            time.sleep(espera)
+        try:
+            elemento = driver.find_element("id", element_id)
+            print(f"[OK] Elemento encontrado pelo ID: {element_id}")
+            return elemento
+        except Exception as e:
+            print(f"[X] Tentativa com ID falhou (espera {espera}s): ",e)
+
+    # Tentativas por XPATH
+    tentativas_xpath = [0, 2, 3]
+    for espera in tentativas_xpath:
+        if espera > 0:
+            time.sleep(espera)
+        try:
+            elemento = driver.find_element("xpath", element_xpath)
+            print(f"[OK] Elemento encontrado pelo XPATH: {element_xpath}")
+            return elemento
+        except Exception as e:
+            print(f"[X] Tentativa com XPATH falhou (espera {espera}s) :", e)
+
+    # Se não encontrou
+    while True:
+        escolha = input("Elemento não encontrado. Digite 1 para sair ou 2 para continuar: ")
+        if escolha == "1":
+            print("Saindo do programa...")
+            exit(0)
+        elif escolha == "2":
+            print("Prosseguindo sem o elemento...")
+            return None
+        else:
+            print("Opção inválida, tente novamente.")
+#preenchendo username
+userName = localizar_elemento(driver, variables.fieldUsername_ID, variables.fieldUsername_XPATH)
+if userName:
+    userName.send_keys(pw.userK)
+    time.sleep(2)
+    # userName.submit() #está enviado com campo vazio
+
+#pressionando botão next
+nextButton = localizar_elemento(driver, variables.buttonNext_ID, variables.buttonNext_XPATH)
+if nextButton:
+    nextButton.click()
+
+#preenchendo password
+time.sleep(2)  
+password = localizar_elemento(driver, variables.fieldPassword_ID, variables.fieldPassword_XPATH)
+if password:
+    password.send_keys(pw.passK)
+    password.submit()   
+
+#pressionando botão Sign in
+time.sleep(2)
+nextButton = localizar_elemento(driver, variables.buttonNext_ID, variables.buttonNext_XPATH)
+if nextButton:
+    nextButton.click()
+time.sleep(5)
+
+#marcar não mostrar novamente e clicar em Yes
+checkboxDontShowAgain = localizar_elemento(driver, variables.checkboxDontShowAgain_ID, variables.checkboxDontShowAgain_XPATH)
+if checkboxDontShowAgain:
+    checkboxDontShowAgain.click()
+    time.sleep(2)
+    nextButton.click()
+
+
+#-------------------------------------- Pós login --------------------------------------
+time.sleep(10)  # espera a página carregar
+# Clica em "Reservas de Recursos"
+buttonReservaDeRecurso = localizar_elemento(driver, variables.buttonReservaDeRecurso_ID, variables.buttonReservaDeRecurso_XPATH)
+if buttonReservaDeRecurso:
+    buttonReservaDeRecurso.click()
+    time.sleep(5)  # espera a página carregar
---
+++ empython/ApontHsNoDynamics/variables.py
@@ -0,0 +1,19 @@
+#TAG: username, campo, nome de usuário
+fieldUsername_ID = 'i0116'
+fieldUsername_XPATH = '//*[@id="i0116"]'
+
+#TAG: password, campo, senha do usuário
+fieldPassword_ID = 'i0118'
+fieldPassword_XPATH = '//*[@id="i0118"]'
+
+#TAG: Botão próximo na pagina de username e o mesmo para sign in na pagina de password e o mesmo para Yes na pagina para manter concetado
+buttonNext_ID = 'idSIButton9'
+buttonNext_XPATH = '//*[@id="idSIButton9"]'
+
+#Tag: Checkbox sim na pagina de manter conectado
+checkboxDontShowAgain_ID = 'KmsiCheckboxField'
+checkboxDontShowAgain_XPATH = '//*[@id="KmsiCheckboxField"]'
+
+#Tag: Reserva de recursos
+buttonReservaDeRecurso_ID = 'Reservas de Recursos'
+buttonReservaDeRecurso_XPATH = '/html/body/div[2]/div[1]/div/div/div[1]/div[4]/div[1]/div/div[2]/ul/li[10]/ul/li[6]/div/div/div[2]/span[2]'
