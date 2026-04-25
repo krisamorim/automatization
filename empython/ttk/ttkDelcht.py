761c3fbd5eeed4a112f24403a13d608a0722e5fb
krisamorim <kris.a.afurtado@gmail.com>
2026-04-25 09:52:56 -0300

    update gpt e ttk del

--- empython/ttk/ttkDelcht.py
+++ empython/ttk/ttkDelcht.py
@@ -1,22 +1,21 @@
 import pyautogui
 from time import sleep
-click_1_telaAOC = [241, 492] #GPT 3 pontinhos
-click_2_telaAOCDELTE = [297, 740] #GPT 3 delete
+click_1_telaAOC = [359, 225] #GPT 3 pontinhos
+click_2_telaAOCDELTE = [253, 318] #GPT 3 delete
 click_3_telaAOCYES = [1156, 592] #GPT 3 yes
 
 def execucao ():
     pyautogui.hotkey('alt','tab')
 
-    for i in range(12):
+    for i in range(50):
         pyautogui.moveTo(click_1_telaAOC)
         sleep(.6)
         pyautogui.click(click_1_telaAOC) #3 pontos
         sleep(.6)
         pyautogui.click(click_2_telaAOCDELTE) #delet
-        sleep(.6)
-        pyautogui.click(click_3_telaAOCYES) #yes
-        print(i)
-        sleep(2)
+    
+        print(f'\rem execução: {i+1}/12', end='')
+        sleep(.7)
     pyautogui.hotkey('alt','tab')
 
 execucao()
